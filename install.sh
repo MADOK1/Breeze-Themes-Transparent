1567d0459c5cda7d14b709b4dd2c06ab9291dd4b
Maddock Pearson <99259670+MADOK1@users.noreply.github.com>
2026-02-25 13:24:38 -0500

    better

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
-mkdir ~/.local/share/desktoptheme
+mkdir ~/.local/share/plasma/desktoptheme
 mkdir ~/.local/share/color-schemes
-cp desktoptheme/* ~/.local/share/desktoptheme -rv
+cp desktoptheme/* ~/.local/share/plasma/desktoptheme -rv
 cp color-schemes/* ~/.local/share/color-schemes/ -rv
